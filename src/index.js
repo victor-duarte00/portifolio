c481b00b568e8acf0425b90f04798fd9c660c931
Victor Duarte <74966069+victor-duarte00@users.noreply.github.com>
2021-12-29 00:20:56 +0000

    changing filenames

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
-import Home from './containers/home';
-import Projects from './containers/projects'
-import Contacts from'./containers/contacts'
+import Home from './containers/home/home';
+import Projects from './containers/projects/projects'
+import Contacts from'./containers/contacts/contacts'
 
 import GlobalStyle from './styles/globalStyle'
-import Navbar from './sub'
+import Navbar from './sub/navbar'
 
 
 
