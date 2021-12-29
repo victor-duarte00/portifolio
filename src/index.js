ef2ba64c1e7fd35dca0c3d6514291e80460ac1e7
Victor Duarte <74966069+victor-duarte00@users.noreply.github.com>
2021-12-29 00:11:54 +0000

    Create Router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Routes from './routes';
+
+import Home from './containers/home';
+import Projects from './containers/projects'
+import Contacts from'./containers/contacts'
+
 import GlobalStyle from './styles/globalStyle'
 import Navbar from './sub'
 
@@ -9,8 +14,16 @@ import Navbar from './sub'
 
 ReactDOM.render(
   <>
-    <Routes />
     <GlobalStyle />
+    <Navbar/>
+    <Router>
+            <Switch>
+              
+                <Route exact path="/" component={Home} />
+                <Route exact path="/projetos" component={Projects} />
+                <Route exact path="/contatos" component={Contacts} />
+            </Switch>
+        </Router>
   </>,
   document.getElementById('root')
 );
